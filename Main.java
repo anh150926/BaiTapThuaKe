a5099f99512a526297e9c0f312fd5d2b6773933f
anh150926 <leanhhv14@gmail.com>
2025-10-07 17:28:59 +0700

    init project

--- Main.java
+++ Main.java
@@ -8,23 +8,19 @@ public class Main {
         FoodProduct fp1 = new FoodProduct("F01", "Bánh quy Oreo", 35000, "10/12/2025");
         FoodProduct fp2 = new FoodProduct("F02", "Sữa Vinamilk", 30000, "20/10/2025");
 
-        // Đơn hàng thanh toán tiền mặt
         Order order1 = new Order("Lưu Hải Anh", new CashPayment());
         order1.addProduct(ep1);
         order1.addProduct(fp1);
         order1.checkout();
 
-        // Đơn hàng thanh toán bằng thẻ tín dụng
         Order order2 = new Order("Vũ Tiến Đạt", new CreditCardPayment());
         order2.addProduct(new ElectronicProduct("E02", "iPhone 15", 28000000, "IMEI8888", 24));
         order2.checkout();
 
-        // Đơn hàng thanh toán qua Momo
         Order order3 = new Order("Nguyễn Lê Anh", new MomoPayment());
         order3.addProduct(fp2);
         order3.checkout();
 
-        // Đơn hàng mới dùng ZaloPay để thanh toán
         Order order4 = new Order("Trần Duy Dũng", new ZaloPayPayment());
         order4.addProduct(fp1);
         order4.checkout();
--- order/Order.java
+++ order/Order.java
@@ -2,7 +2,11 @@ package order;
 
 import java.util.ArrayList;
 import model.Product;
+import payment.CashPayment;
+import payment.CreditCardPayment;
+import payment.MomoPayment;
 import payment.Payment;
+import payment.ZaloPayPayment;
 
 public class Order {
     private String customerName;
@@ -29,6 +33,19 @@ public class Order {
     public void checkout() {
         double total = getTotalAmount();
         System.out.println("Khách hàng: " + customerName + ". Tổng tiền: " + total + " VND.");
+
         payment.pay(total);
+
+        if (payment instanceof CashPayment) {
+            System.out.println("Đơn hàng được thanh toán trực tiếp tại quầy.\n");
+        } else if (payment instanceof CreditCardPayment) {
+            System.out.println("Giao dịch đã được xác thực bởi ngân hàng phát hành.\n");
+        } else if (payment instanceof MomoPayment) {
+            System.out.println("Giao dịch được xử lý qua cổng Momo Pay.\n");
+        } else if (payment instanceof ZaloPayPayment) {
+            System.out.println("Xác nhận thanh toán qua ví ZaloPay.\n");
+        } else {
+            System.out.println("Không xác định phương thức thanh toán.\n");
+        }
     }
 }
